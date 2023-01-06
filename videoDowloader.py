5bab7ac523b0937c1768db1f5f7166370f9ea38e
n0-L1mit <andriy.golov13@gmail.com>
2023-01-06 18:25:24 +0100

    test commit

---
+++ videoDowloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+
+def Download(link):
+    youtubeLink = YouTube(link, on_progress_callback=on_progress)
+    youtubeObject = youtubeLink.streams.get_highest_resolution()
+    
+    try:
+        youtubeObject.download()
+    except:
+        print("There has been an error in downloading your youtube video")
+    print("\nThe download was successful\n")
+    
+link = input("URL:\n")
+Download(link)
